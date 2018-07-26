7c3ad8c9c98bd5a59812e0ac1b4a47a4de09ffc9
Matthew Henderson <hendersonmpa@gmail.com>
2018-07-26 14:22:47 -0400

    prior to merge

--- create_reports.r
+++ create_reports.r
@@ -8,8 +8,8 @@ render_document <- function(analyte, file) {
     rmarkdown::render(Rmd_template, params = list(analyte = analyte, file = file))
 }
 
-render_query <- function() {
-    rmarkdown::render(Rmd_template, params = "ask")}
+## render_query <- function() {
+##     rmarkdown::render(Rmd_template, params = "ask")}
 
 rename_document <- function(analyte){
     file.rename(from = pdf_output,
@@ -24,3 +24,4 @@ create_report <- function(analyte, file){
 create_report(analyte = "TSH", file = "../data/TSH_GSP_validation.xls")
 create_report(analyte = "IRT", file = "../data/IRT_GSP_validation.xls")
 create_report(analyte = "N17P", file = "../data/N17P_GSP_validation.xls")
+
