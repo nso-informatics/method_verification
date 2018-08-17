895480805959a02c0f4515f50dfdf2f4c628ec55
Matthew Henderson <hendersonmpa@gmail.com>
2018-08-17 12:39:02 -0400

    Working version of the reports

--- create_reports.r
+++ create_reports.r
@@ -4,8 +4,10 @@ library("rmarkdown")
 Rmd_template <- "method_verification_report.Rmd"
 pdf_output <- "method_verification_report.pdf"
 
-render_document <- function(analyte, file) {
-    rmarkdown::render(Rmd_template, params = list(analyte = analyte, file = file))
+render_document <- function(analyte, file, recommendation) {
+    rmarkdown::render(Rmd_template, params = list(analyte = analyte,
+                                                  file = file,
+                                                  recommendation = recommendation))
 }
 
 ## render_query <- function() {
@@ -16,12 +18,11 @@ rename_document <- function(analyte){
                 to = paste0("../reports/", analyte, "_GSP_Method_Validation", ".pdf"))
 }
 
-create_report <- function(analyte, file){
-    render_document(analyte, file)
+create_report <- function(analyte, file, recommendation){
+    render_document(analyte, file, recommendation)
     rename_document(analyte)
 }
 
-create_report(analyte = "TSH", file = "../data/TSH_GSP_validation.xls")
-create_report(analyte = "IRT", file = "../data/IRT_GSP_validation.xls")
-create_report(analyte = "N17P", file = "../data/N17P_GSP_validation.xls")
-
+create_report(analyte = "TSH", file = "../data/TSH_GSP_validation.xls", recommendation = "acceptable")
+create_report(analyte = "IRT", file = "../data/IRT_GSP_validation.xls", recommendation = "acceptable")
+create_report(analyte = "N17P", file = "../data/N17P_GSP_validation.xls", recommendation = "acceptable")
